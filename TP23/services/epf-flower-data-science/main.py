13cd3f3d6ea023bbe70a56c7fb8d00b52faaaad6
MOHAMOUD ROBLEH Anes <anesos2000@gmail.com>
2024-11-26 16:55:07 +0100

    step6

--- TP23/services/epf-flower-data-science/main.py
+++ TP23/services/epf-flower-data-science/main.py
@@ -1,8 +1,12 @@
 import uvicorn
 
 from src.app import get_application
+from fastapi.responses import RedirectResponse
 
 app = get_application()
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
